b619526ad4fb2246552d0be871f20534c2058e01
Chamath Wanigasooriya <chamathwanigasooriya@gmail.com>
2020-10-26 23:48:01 +0530

    hosting

--- RMSPythonApp/notifyapp.py
+++
@@ -1,5 +0,0 @@
-from win10toast import ToastNotifier 
-def Notify(message):
-    toaster = ToastNotifier()
-    toaster.show_toast("Python Notification", message, duration=10, 
-    icon_path="Paomedia-Small-N-Flat-Bell.ico")
--- RMSPythonApp/web/app.js
+++
@@ -1,8 +0,0 @@
-"use strict"; 
-class PyApp{
-call_py_listener(email){
-    console.log("Entered javascript function");
-    eel.listener(email);
-}
-}
-export default PyApp;
