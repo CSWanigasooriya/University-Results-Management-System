2246a9d0eaeb271c2b2e0b7e1db8e94a5cf57433
Chamath Wanigasooriya <chamathwanigasooriya@gmail.com>
2020-10-29 19:00:46 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,6 @@ export const environment = {
   PHP_API_SERVER: 'http://localhost:80/backend',
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyCd0SGZg_599s1k0WTHhEXoBcWhNHXdWaY',
-    authDomain: 'result-management-system-b3f8b.firebaseapp.com',
-    databaseURL: 'https://result-management-system-b3f8b.firebaseio.com',
-    projectId: 'result-management-system-b3f8b',
-    storageBucket: 'result-management-system-b3f8b.appspot.com',
-    messagingSenderId: '523016644319',
-    appId: '1:523016644319:web:15d612982ddc0cd19c6b6a',
-    measurementId: 'G-B120G5CJLD'
   }
 };
 
